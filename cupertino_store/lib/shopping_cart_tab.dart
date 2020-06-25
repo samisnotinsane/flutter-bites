249ec181529606bde515655717d74afe3c2271c5
samisnotinsane <sameen11@hotmail.com>
2020-06-25 23:39:20 +0100

    Adding 'Add customer info'.

--- cupertino_store/lib/shopping_cart_tab.dart
+++ cupertino_store/lib/shopping_cart_tab.dart
@@ -9,6 +9,115 @@ class ShoppingCartTab extends StatefulWidget {
 }
 
 class _ShoppingCartTabState extends State<ShoppingCartTab> {
+  String name;
+  String email;
+  String location;
+  String pin;
+  DateTime dateTime = DateTime.now();
+
+  Widget _buildNameField() {
+    return CupertinoTextField(
+      prefix: Icon(
+        CupertinoIcons.person_solid,
+        color: CupertinoColors.lightBackgroundGray,
+        size: 28,
+      ),
+      padding: EdgeInsets.symmetric(horizontal: 6, vertical: 12),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      textCapitalization: TextCapitalization.words,
+      autocorrect: false,
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 0,
+            color: CupertinoColors.inactiveGray,
+          ),
+        ),
+      ),
+      placeholder: 'Name',
+      onChanged: (newName) {
+        setState(() {
+          name = newName;
+        });
+      },
+    );
+  }
+
+  Widget _buildEmailField() {
+    return CupertinoTextField(
+      prefix: Icon(
+        CupertinoIcons.mail_solid,
+      ),
+      padding: EdgeInsets.symmetric(
+        horizontal: 6,
+        vertical: 12,
+      ),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      keyboardType: TextInputType.emailAddress,
+      autocorrect: false,
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 0,
+            color: CupertinoColors.inactiveGray,
+          ),
+        ),
+      ),
+      placeholder: 'Email',
+    );
+  }
+
+  Widget _buildLocationField() {
+    return CupertinoTextField(
+      prefix: Icon(
+        CupertinoIcons.location_solid,
+        color: CupertinoColors.lightBackgroundGray,
+        size: 28,
+      ),
+      padding: EdgeInsets.symmetric(
+        horizontal: 6,
+        vertical: 12,
+      ),
+      clearButtonMode: OverlayVisibilityMode.editing,
+      textCapitalization: TextCapitalization.words,
+      decoration: BoxDecoration(
+        border: Border(
+          bottom: BorderSide(
+            width: 0,
+            color: CupertinoColors.inactiveGray,
+          ),
+        ),
+      ),
+      placeholder: 'Location',
+    );
+  }
+
+  SliverChildBuilderDelegate _buildSliverChildBuilderDelegate(
+      AppStateModel model) {
+    return SliverChildBuilderDelegate((context, index) {
+      switch (index) {
+        case 0:
+          return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: _buildNameField(),
+          );
+        case 1:
+          return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: _buildEmailField(),
+          );
+        case 2:
+          return Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16),
+            child: _buildLocationField(),
+          );
+        default:
+          // Do nothing. For now.
+      }
+      return null;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Consumer<AppStateModel>(builder: (context, model, child) {
@@ -16,6 +125,13 @@ class _ShoppingCartTabState extends State<ShoppingCartTab> {
         slivers: <Widget>[
           CupertinoSliverNavigationBar(
             largeTitle: Text('Shopping Cart'),
+          ),
+          SliverSafeArea(
+            top: false,
+            minimum: EdgeInsets.only(top: 4),
+            sliver: SliverList(
+              delegate: _buildSliverChildBuilderDelegate(model),
+            ),
           ),
         ],
       );
